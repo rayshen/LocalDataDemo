159790a5a4bc6aec014c69d7eb50c4e79b13e4d2
rayshen <rayshen1012@gmail.com>
2015-10-26 10:48:15 +0800

    Initial Commit

---
+++ LocalDataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalDataDemo
+//
+//  Created by shen on 15/10/26.
+//  Copyright © 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
